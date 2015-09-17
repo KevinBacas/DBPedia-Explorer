09a8e1edcaf1422b67e4bb82f623e674622f6a9c
KevinBacas <bacas.kevin@hotmail.fr>
2015-09-17 12:08:19 -0700

    Removed useless code

--- src/reducers.js
+++ src/reducers.js
@@ -7,17 +7,9 @@ const initialState = {
   error_message: undefined
 };
 
-let qq = true;
-
 function requestApp(state = initialState, action) {
   switch (action.type) {
   case REQUEST_MAKE:
-    if(qq) {
-      setTimeout(() => {
-        store.dispatch(requestFailed("There is an error"));
-      }, 1000);
-      qq = false;
-    }
     return Object.assign({}, state, {
       request_string: action.request_string,
       request_status: action.request_status
