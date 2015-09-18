47be896ee78149e5883ee8c9bac2801908536861
KevinBacas <bacas.kevin@hotmail.fr>
2015-09-18 13:32:25 -0700

    Updated reducer for making request

--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,6 @@
-import { REQUEST_MAKE, REQUEST_COMPLETED, REQUEST_FAILED, requestFailed } from './actions';
+import { REQUEST_MAKE, REQUEST_COMPLETED, REQUEST_FAILED, requestFailed, requestCompleted } from './actions';
 import store from './store';
+import $ from 'jquery';
 
 const initialState = {
   request_string: "",
@@ -10,11 +11,20 @@ const initialState = {
 function requestApp(state = initialState, action) {
   switch (action.type) {
   case REQUEST_MAKE:
+    $.get('http://fr.dbpedia.org/sparql?query='+action.request_string+"&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on")
+    .done((data) => store.dispatch(requestCompleted(data)))
+    .fail((error) => store.dispatch(requestFailed(error)));
+
     return Object.assign({}, state, {
       request_string: action.request_string,
       request_status: action.request_status
     });
   case REQUEST_FAILED:
+    return Object.assign({}, state, {
+      error_message: action.error_message,
+      request_status: action.request_status
+    });
+  case REQUEST_COMPLETED:
     return Object.assign({}, state, {
       error_message: action.error_message,
       request_status: action.request_status
